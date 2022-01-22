a70a4c2d8eb6c61a46ab6666ce76285e083210f3
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2022-01-22 21:08:52 +0530

    changes in student

--- Student.java
+++ Student.java
@@ -1,5 +1,30 @@
 package com.jit.in;
 
 public class Student {
+	
+	private Integer sid;
+	private String sname;
+	private String saddrs;
+	
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSaddrs() {
+		return saddrs;
+	}
+	public void setSaddrs(String saddrs) {
+		this.saddrs = saddrs;
+	}
+	
+	
 
 }
